4ac2b84e034d2062dfa85d69a0896acb57675325
gaaaabz <gaby8gomes@gmail.com>
2025-08-27 10:29:25 -0300

    crud implementado

--- Habitus/src/main/java/Fiap/Habitus/controller/CategoryController.java
+++ Habitus/src/main/java/Fiap/Habitus/controller/CategoryController.java
@@ -2,25 +2,77 @@ package Fiap.Habitus.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import Fiap.Habitus.model.Category;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
+@RequestMapping("categories")
 @RestController
 public class CategoryController {
 
-    @GetMapping("/categories")
-    public List<Category> index(){
+    private List<Category> repository = new ArrayList<>();
 
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1L, "Apartamento", "apartamento"));
-        categories.add(new Category(2L, "Casa", "Casa"));
-        categories.add(new Category(3L, "Moveis", "mobilia"));
+    @GetMapping("{id}")
+    public ResponseEntity<Category> get(@PathVariable Long id){
+        log.info("procurando categoria por id " + id);
+        var categoryFound = getCategoryById(id); 
 
-        return categories;
+        if (categoryFound.isEmpty()) return ResponseEntity.notFound().build(); 
+
+        return ResponseEntity.ok(categoryFound.get());
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Category create(@RequestBody Category category){
+        category.setId(Math.abs(new Random().nextLong()));
+        log.info("criando a categoria " + category);
+        repository.add(category);
+        return category;
+    }
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> destroy(@PathVariable Long id){
+        log.info("excluindo categoria pelo id {}", id);
+        var categoryFound = getCategoryById(id);    
+        if (categoryFound.isEmpty()) return ResponseEntity.notFound().build(); 
+        repository.remove(categoryFound.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Category> update(@RequestBody Category categoryUpdated, @PathVariable Long id ){
+        log.info("atualizando categoria {} pelo id {}", categoryUpdated, id);
+        var categoryFound = getCategoryById(id); 
         
+        if (categoryFound.isEmpty()) return ResponseEntity.notFound().build();
+        repository.remove(categoryFound.get());
+        categoryUpdated.setId(id);
+        repository.add(categoryUpdated);
+
+        return ResponseEntity.ok(categoryUpdated);
+    }
+
+    private Optional<Category> getCategoryById(Long id) {
+        var categoryFound = repository.stream()
+            .filter(category -> category.getId().equals(id))
+            .findFirst();
+        return categoryFound;
     }
     
 }
